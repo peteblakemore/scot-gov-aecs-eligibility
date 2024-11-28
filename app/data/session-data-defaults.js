a3e3a79addc1fb6f9d54cdf08efeb8d57bdd5b48
Pete Blakemore <contact@peteblakemore.co.uk>
2024-11-28 12:02:17 +0000

    Version 4 - WFP audit options

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,4 +2,71 @@ module.exports = {
 
   // Insert values here
 
+  'audit-to-options-mapping': {
+    'Carbon': [
+      'Wild Bird Seed for Farmland Birds',
+      'Stubbles Followed by Green Manure in an Arable Rotation',
+      'Species-rich Grassland Management',
+      'Wetland Management',
+      'Lowland Bog Management',
+      'Management of Buffer Areas for Fens and Lowland Bogs',
+      'Ancient Wood Pasture',
+      'Organic Farming: Conversion',
+      'Organic Farming: Maintenance'
+    ],
+    'Biodiversity': [
+      'Wild Bird Seed for Farmland Birds',
+      'Forage Brassica Crops for Farmland Birds',
+      'Beetle Banks',
+      'Grass Strips in Arable Fields',
+      'Species-rich Grassland Management',
+      'Wader and Wildlife Mown Grassland',
+      'Wader Grazed Grassland',
+      'Heath Management (Coastal, Serpentine, Lowland and Special Interest)',
+      'Wetland Management',
+      'Lowland Bog Management',
+      'Management of Buffer Areas for Fens and Lowland Bogs',
+      'Habitat Mosaic Management',
+      'Managing Scrub of Conservation Value',
+      'Ancient Wood Pasture',
+      'Conservation Management of Small Units',
+      'Control of Invasive Non-native Plant Species'
+    ],
+    'Soil Management': [
+      'Stubbles Followed by Green Manure in an Arable Rotation',
+      'Retention of Winter Stubbles for Wildlife and Water Quality',
+      'Grass Strips in Arable Fields',
+      'Converting Arable at Risk of Erosion or Flooding to Low-input Grassland',
+      'Species-rich Grassland Management',
+      'Hard Standings for Troughs and Gateways',
+      'Livestock Tracks',
+      'Organic Farming: Conversion',
+      'Organic Farming: Maintenance'
+    ],
+    'Water Management': [
+      'Water Margins in Arable Fields',
+      'Water Margins in Grassland Fields',
+      'Retention of Winter Stubbles for Wildlife and Water Quality',
+      'Converting Arable at Risk of Erosion or Flooding to Low-input Grassland',
+      'Wetland Management',
+      'Management of Buffer Areas for Fens and Lowland Bogs',
+      'Alternative Watering',
+      'Rural Sustainable Drainage Systems – Swales',
+      'Rural Sustainable Drainage Systems – Wetland',
+      'Rural Sustainable Drainage Systems – Retention Pond',
+      'Rural Sustainable Drainage Systems – Sediment Traps and Bunds',
+      'Water-use Efficiency – Irrigation Lagoon',
+      'Managing Steading Drainage and Rural Sustainable Drainage Systems',
+      'Pesticide Handling Facilities'
+    ],
+    'Air Quality': [
+      'Species-rich Grassland Management',
+      'Lowland Bog Management',
+      'Management of Buffer Areas for Fens and Lowland Bogs',
+      'Managing Steading Drainage and Rural Sustainable Drainage Systems',
+      'Organic Farming: Conversion',
+      'Organic Farming: Maintenance'
+    ]
+  }
+
 }
--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,10 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/version-4/scheme-options-results_holding-number', function(req, res) {
+    // Store the selected audit areas in session
+    req.session.data['wfp-audit'] = req.body['wfp-audit'];
+    res.render('version-4/scheme-options-results_holding-number');
+});
+
